bdd94baf10dc42e69f37b98dee9c73b6ad6e5dbf
Liubov Shcherbata <lyubovshcherb@gmail.com>
2025-06-27 14:13:57 +0300

    functions add, remove, find added to model.js

--- HomeWorkAnswers/Lesson_8/js/model.js
+++ HomeWorkAnswers/Lesson_8/js/model.js
@@ -1,34 +1,63 @@
-const allBooks={
-    books:[
+const allBooks = {
+    books: [
         {
-        "title": "1984",
-        "author": "Джордж Орвелл",
-        "year": 1949,
-        "genre": "Антиутопія"
+            "id": 1,
+            "title": "1984",
+            "author": "Джордж Орвелл",
+            "year": 1949,
+            "genre": "Антиутопія"
+        },
+        {
+            "id": 2,
+            "title": "Гаррі Поттер і філософський камінь",
+            "author": "Дж. К. Роулінг",
+            "year": 1997,
+            "genre": "Фентезі"
+        },
+        {
+            "id": 3,
+            "title": "Гра престолів",
+            "author": "Джордж Р. Р. Мартін",
+            "year": 1996,
+            "genre": "Фентезі"
+        },
+        {
+            "id": 4,
+            "title": "Старий і море",
+            "author": "Ернест Хемінгуей",
+            "year": 1952,
+            "genre": "Пригодницький роман"
+        },
+        {
+            "id": 5,
+            "title": "Майстер і Маргарита",
+            "author": "Михайло Булгаков",
+            "year": 1967,
+            "genre": "Фантастика"
+        }
+    ],
+
+    lastId: 5,
+
+    add(book) {
+        this.lastId++;
+        book.id = this.lastId;
+        this.books.push(book);
     },
-    {
-        "title": "Гаррі Поттер і філософський камінь",
-        "author": "Дж. К. Роулінг",
-        "year": 1997,
-        "genre": "Фентезі"
+
+    remuve(id) {
+        let index = this.books.findIndex(x => x.id == id);
+        this.books.splice(index, 1);
     },
-    {
-        "title": "Гра престолів",
-        "author": "Джордж Р. Р. Мартін",
-        "year": 1996,
-        "genre": "Фентезі"
+
+    update(id, book) {
+        let index = this.books.findIndex(x => x.id == id);
+        this.books[index] = book;
     },
-    {
-        "title": "Старий і море",
-        "author": "Ернест Хемінгуей",
-        "year": 1952,
-        "genre": "Пригодницький роман"
-    },
-    {
-        "title": "Майстер і Маргарита",
-        "author": "Михайло Булгаков",
-        "year": 1967,
-        "genre": "Фантастика"
+
+    find(id) {
+        let index = this.books.findIndex(x => x.id == id);
+        return this.books[index];
     }
-    ]
+
 }
