bacf471dff38bdb96d7c8a7ada902ccc206d28e5
Liubov Shcherbata <lyubovshcherb@gmail.com>
2025-06-27 14:50:57 +0300

    add book function added

--- HomeWorkAnswers/Lesson_8/js/controller.js
+++ HomeWorkAnswers/Lesson_8/js/controller.js
@@ -1,2 +1,27 @@
 let html=document.querySelector("#book-list");
 bookList.render(allBooks.books, html);
+
+//inputs
+const titleInput=document.querySelector("#titleInput");
+const authorInput=document.querySelector("#authorInput");
+const yearInput=document.querySelector("#yearInput");
+const genreInput=document.querySelector("#genreInput");
+
+function render(){
+    bookList.render(allBooks.books, html);
+}
+
+contactForm.addEventListener("submit", function(e){
+    e.preventDefault(); // Щоб форма не перезавантажувала сторінку
+    let book={
+        title: titleInput.value,
+        author: authorInput.value,
+        year: yearInput.value,
+        genre: genreInput.value,
+    }
+    allBooks.add(book);
+    html.innerHTML = ""; // Щоб не дублювалися книги
+    render();
+});
+
+render();
